8876fa3286d82bdcbb4d6020fbeaf42f0e7208db
Cees-Jan Kiewiet <ceesjank@gmail.com>
2023-03-07 11:56:36 +0100

    Take the first of all any one ofs

--- src/Schema/Deployment/Payload.php
+++ src/Schema/Deployment/Payload.php
@@ -1,13 +1,13 @@
 <?php
 
 declare (strict_types=1);
-namespace ApiClients\Client\GitHubEnterprise\Schema\Operation\CodeScanning\GetAnalysis\Response\ApplicationjsonPlusSarif;
+namespace ApiClients\Client\GitHubEnterprise\Schema\Deployment;
 
 use ApiClients\Client\GitHubEnterprise\Hydrator;
 use ApiClients\Client\GitHubEnterprise\Operation;
 use ApiClients\Client\GitHubEnterprise\Schema;
 use ApiClients\Client\GitHubEnterprise\WebHook;
-final readonly class H200
+final readonly class Payload
 {
     public const SCHEMA_JSON = '{"type":"object","additionalProperties":true}';
     public const SCHEMA_TITLE = '';
