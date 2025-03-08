ebe83d6983606dc8de0d7f9e9fe96e29b87620f7
WyriHaximus <147145+WyriHaximus@users.noreply.github.com>
2025-03-08 10:50:23 +0000

    [GitHubEnterprise-3.15] Update to 1.1.4-0e6a4a1bd0678c8ca272e91b4354bf3a from 1.1.4-531e13661f965ac04f0ddfe589e8a768 Detected Schema changes:
    ```
    ├─┬Paths
    │ ├─┬/orgs/{org}/secret-scanning/alerts
    │ │ └─┬GET
    │ │   └─┬Parameters
    │ │     └──[🔀] description (217454:20)
    │ ├─┬/repos/{owner}/{repo}/code-scanning/alerts/{alert_number}
    │ │ └─┬PATCH
    │ │   ├─┬Responses
    │ │   │ └──[➖] codes (218606:7)❌
    │ │   └─┬Requestbody
    │ │     └─┬application/json
    │ │       ├─┬Schema
    │ │       │ └──[➖] properties (26737:17)❌
    │ │       └─┬default
    │ │         ├──[🔀] value (26742:19)
    │ │         ├──[🔀] value (26742:19)
    │ │         ├──[🔀] value (26742:19)
    │ │         └──[➖] value (26744:19)
    │ ├─┬/repos/{owner}/{repo}/secret-scanning/alerts
    │ │ └─┬GET
    │ │   └─┬Parameters
    │ │     └──[🔀] description (217454:20)
    │ └─┬/enterprises/{enterprise}/secret-scanning/alerts
    │   └─┬GET
    │     └─┬Parameters
    │       └──[🔀] description (217454:20)
    └─┬Components
      ├──[➖] schemas (92411:7)❌
      ├──[➖] schemas (82334:7)❌
      ├──[➖] schemas (92373:7)❌
      ├──[➕] schemas (92365:7)
      ├──[➕] schemas (92403:7)
      └─┬exemption-request
        ├─┬metadata
        │ └─┬ONEOF
        │   └──[🔀] $ref (92403:14)❌
        └─┬exemption_request_data
          └─┬ONEOF
            └──[🔀] $ref (92365:14)❌
    
    ```
    
    | Document Element | Total Changes | Breaking Changes |
    |------------------|---------------|------------------|
    | paths            | 9             | 2                |
    | components       | 7             | 5                |
    
    Date: 03/08/25 | Commit: Original: etc/specs/GitHubEnterprise-3.15/current.spec.yaml, Modified: etc/specs/GitHubEnterprise-3.15/previous.spec.yaml,
    
    - ❌ **BREAKING Changes**: _7_ out of _16_
    - **Modifications**: _8_
    - **Removals**: _6_
    - **Additions**: _2_
    - **Breaking Removals**: _5_
    - **Breaking Modifications**: _2_
    
    ERROR: breaking changes discovered

--- src/Schema/DismissalRequestSecretScanningMetadata.php
+++ src/Schema/DismissalRequestSecretScanningMetadata.php
@@ -6,7 +6,7 @@ namespace ApiClients\Client\GitHubEnterprise\Schema;
 
 use EventSauce\ObjectHydrator\MapFrom;
 
-final readonly class ExemptionRequestSecretScanningClosureMetadata
+final readonly class DismissalRequestSecretScanningMetadata
 {
     public const SCHEMA_JSON         = '{
     "title": "Secret scanning alert dismissal request metadata",
